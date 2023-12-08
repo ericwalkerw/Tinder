93c1be97b1e0b992677d54efe5cccd355712e8c2
ericwalkerw <ericwalker@gmail.com>
2023-12-08 17:52:14 +0700

    add connection text

--- TinderApp/assets/Client/Scripts/Client.js
+++ TinderApp/assets/Client/Scripts/Client.js
@@ -5,15 +5,26 @@ cc.Class({
     mApp: require("appManager"),
     serverUri: "https://socket-chat-app-ce41.onrender.com/",
     _socket: WebSocket,
+    userOnline:cc.Label,
+    userConnected:cc.Label,
   },
 
   start() {
     this._socket = io(this.serverUri);
-    this._socket.on("connect", (data) => {
-      cc.log("connected");
-    });
     this._socket.on("chat", (data) => {
       this.mApp.onCreateCustomMes(data, this._socket.id);
+    });
+    this._socket.on("login", (data) => {
+      cc.log(data);
+      this.userConnected.string = `${data.userName}`;
+      const seq = cc.sequence(  
+        cc.moveTo(1,206,800),
+        cc.delayTime(1),
+        cc.moveTo(1,560,800),
+      )
+      this.userConnected.node.stopAllActions();
+      this.userConnected.node.runAction(seq);
+      this.userOnline.string = `Online: ${data.numberUsers}`
     });
   },
 
@@ -26,8 +37,8 @@ cc.Class({
 
   senduserData() {
     const userData = this.mUser.onLogin();
-    if(userData.userName.trim()!= ""){
-      this._socket.emit("user", userData);
+    if (userData.userName.trim() != "") {
+      this._socket.emit("login", userData);
     }
   },
 });
